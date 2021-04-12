35ee44ee9d880b66ec12e3468fb976e5532615f4
Dmfeuerman <dmfeuerman@loyola.edu>
2021-04-12 13:00:09 -0400

    version 1.0.2

--- FinalProject.py
+++ FinalProject.py
@@ -1,10 +1,15 @@
-##
+##  Dylan Feuerman, JackToohey and Naylah Perodin
+## Group Project
+## 4/12/2021
+## CS-484-01
+## Simari
 
 import numpy as np
 
 def loadText(file):
 
-    data = np.loadtxt(file, dtype=str, delimiter=";", skiprows=1)
+    data = np.loadtxt(file, dtype=str, delimiter=";", skiprows=1, unpack=True)
+    print(data[32])
     return data
 
 
