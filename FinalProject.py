4f400ff9bf820d01415b980e7845bfd348bd09e9
njperodin <42972102+njperodin@users.noreply.github.com>
2021-04-16 13:32:48 -0400

    created data frame, created training and testing data for x and y
    created data frame, created training and testing data for x and y

--- FinalProject.py
+++ FinalProject.py
@@ -20,16 +20,35 @@ def loadText(file):
 
     return data
 
+#create DataFrame
+def createDataFrame(file):
+    
+    data = pd.read_csv(file,delimiter=";")
+    
+    return data
 
 def createXData(data):
 
     return data[TARGET]
 
-
 def createYData(data):
 
     return data[:INPUT_DATA]
 
+# retrieve the x and y data
+def create_x_y(data):
+    
+    y = data["G3"]
+    
+    x = []
+    for i in range(data.shape[0]):
+        column_data =[]
+        for column in data.columns:
+            column_data.append(data[str(column)][i])
+
+        x.append(column_data)
+        
+    return x, y
 
 def printShape(data):
 
@@ -51,6 +70,12 @@ def Training_Data(X, Y):
 
     return X_train, X_test, y_train, y_test
 
+# create the training and testing data for x and y
+def create_train_test_data(x, y):
+    
+    X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.25, random_state=3)
+    
+    return X_train, X_test, y_train, y_test
 
 def main():
 
@@ -58,10 +83,21 @@ def main():
 
     Y = createYData(data)
     X = createYData(data)
-
+    
+    
     X_train, X_test, y_train, y_test = Training_Data(X, Y)
-
+   
     list_csv = CSV_to_list("student\student-mat.csv")
+    
+    # create dataframe
+    file_data = createDataFrame("student\student-mat.csv")
+    
+    # retrieve the x and y data
+    x, y = create_x_y(file_data)
+    
+    # create the training and testing data for x and y
+    X_train, X_test, y_train, y_test = create_train_test_data(x, y)
+    print(X_train, X_test, y_train, y_test)
 
 
 main()
